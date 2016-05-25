e70d93a9bf45fc39f303388561c2176738bc15b1
Arnaud Lemaire <alemaire@norad.fr>
2016-05-25 10:04:49 +0200

    install go-bindata before build

--- build.sh
+++ build.sh
@@ -2,8 +2,14 @@
 set -e
 dir=$( dirname "$0" )
 
-[ -f ${GOPATH}/bin/go-bindata ] || (go get github.com/jteeuwen/go-bindata && make -C ${GOPATH}/src/github.com/jteeuwen/go-bindata)
+[ -f ${GOPATH}/bin/go-bindata ] || {
+    go get github.com/jteeuwen/go-bindata || true
+    cd ${GOPATH}/src/github.com/jteeuwen/go-bindata
+    git checkout v2.0.3
+    go build
+    cd -
+}
 
 
-go-bindata -nomemcopy -pkg main -o ${dir}/bindata.go ${dir}/templates/...
+go-bindata -nomemcopy -pkg main -out ${dir}/bindata.go ${dir}/templates/...
 godep go build
